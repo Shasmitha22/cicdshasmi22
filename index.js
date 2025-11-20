5be96ca0e1ad215714a45fd5a0325102cd539412
Shasmitha22 <shasmitha22110312@snuchennai.edu.in>
2025-11-21 03:19:12 +0530

    Refactor server to use constants for PORT and HOST

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const http = require('http');
 
+const PORT = 3000;
+const HOST = '0.0.0.0';   // IMPORTANT: ECS needs this
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.end("Hello from CI/CD Pipeline with Jenkins + Docker + AWS!");
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
