9d9aa9191c4d88e6086a35953eba68451eaada7a
Shasmitha22 <shasmitha22110312@snuchennai.edu.in>
2025-11-21 04:10:05 +0530

    Update response message in server to 'Hello CI CD Pipeline'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const HOST = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("1111111111111111111 shasmitha Pipeline with Jenkins + Docker + AWS!");
+  res.end("Hello CI CD Pipeline with Jenkins + Docker + AWS!");
 });
 
 server.listen(PORT, HOST, () => {
