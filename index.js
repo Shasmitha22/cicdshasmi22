2e7e9b11bd631035da879b2251414f78418d6020
Shasmitha22 <shasmitha22110312@snuchennai.edu.in>
2025-11-21 10:53:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const HOST = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello ci cd ");
+  res.end("Hello Maam");
 });
 
 server.listen(PORT, HOST, () => {
